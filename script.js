20e1f93e673175ee14b08e2bba07cbc48db28c99
Beri Alemu <berialemu8@gmail.com>
2024-08-03 12:47:45 +0300

    the search button appears and disapears when the search button is touched

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
 const header = document.querySelector(".header");
 const search = document.querySelector(".search");
+const searchBtn = document.querySelector(".search-bar");
 
-search.addEventListener("keyup", () => {
-    header.innerHTML = search.value;
+let i = true
+searchBtn.addEventListener("click", () => {
+    if (i) {
+        search.style.display = "block"
+        i = false
+    } else if (!i) {
+        search.style.display = "none"
+        i = true
+    }
 })
