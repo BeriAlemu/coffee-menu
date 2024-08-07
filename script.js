8c8e561d859dc8b2c0c07f8a04a05211052bb816
Beri Alemu <berialemu8@gmail.com>
2024-08-07 18:53:34 +0300

    added a cool clock that is functional with java script

--- script.js
+++ script.js
@@ -12,3 +12,20 @@ searchBtn.addEventListener("click", () => {
         i = true
     }
 })
+
+var second = 0;
+var minute = 0;
+var hour = 0;
+var d = new Date();
+
+setInterval(
+    function(){
+        d = new Date();
+        second = d.getSeconds() * 6;
+        minute = d.getMinutes() * 6;
+        hour = d.getMinutes() * 0.5;
+        document.querySelector(".analog-sec").style.transform = "rotate(" + second + "deg)";
+        document.querySelector(".analog-min").style.transform = "rotate(" + minute + "deg)";
+        document.querySelector(".analog-hour").style.transform = "rotate(" + hour + "deg)";
+    }, 1000
+);
